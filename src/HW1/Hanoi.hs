49fc064529d21de32dbdd1fe77919a2bd8fd7e70
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-02-23 01:07:51 -0600

    Add another test for hanoi, rename test script

--- src/HW1/Hanoi.hs
+++ src/HW1/Hanoi.hs
@@ -22,6 +22,9 @@ type Move = (Peg, Peg)
 --
 -- >>> hanoi 2 "a" "b" "c"
 -- [("a","c"),("a","b"),("c","b")]
+--
+-- >>> hanoi 3 "a" "b" "c"
+-- [("a","b"),("a","c"),("b","c"),("a","b"),("c","a"),("c","b"),("a","b")]
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 0 _ _ _ = []
 hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
