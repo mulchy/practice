dda06f6fe09fe929a2cd5e1c35db73ba5811cecd
Ryan Mulcahy <ryan.mulcahy@code42.com>
2015-02-22 23:54:04 -0600

    Add solution to tower of hanoi

---
+++ src/HW1/Hanoi.hs
@@ -0,0 +1,27 @@
+-- |
+-- Tower of Hanoi
+module Hanoi where
+
+-- |
+-- The name of a peg
+type Peg = String
+
+-- |
+-- Represents a move from peg fst to snd
+type Move = (Peg, Peg)
+
+-- |
+-- Generates a list of moves needed to move n discs from the first peg
+-- to the second peg where a, b, and c are the names of the pegs
+--
+-- >>> hanoi 0 "a" "b" "c"
+-- []
+--
+-- >>> hanoi 1 "a" "b" "c"
+-- [("a","b")]
+--
+-- >>> hanoi 2 "a" "b" "c"
+-- [("a","c"),("a","b"),("c","b")]
+hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
+hanoi 0 _ _ _ = []
+hanoi n a b c = (hanoi (n-1) a c b) ++ [(a,b)] ++ (hanoi (n-1) c b a)
--- test/doctests.hs
+++ test/doctests.hs
@@ -3,4 +3,4 @@ module Main where
 import Test.DocTest
 
 main :: IO ()
-main = doctest ["src/HW1/CreditCard.hs"]
+main = doctest ["src/HW1/CreditCard.hs", "src/HW1/Hanoi.hs"]
